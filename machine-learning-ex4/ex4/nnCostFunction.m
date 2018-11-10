05bc997d7da007d02b085c1427371d9f8fcb24f7
Aarav <pandya.aarav.97@gmail.com>
2018-11-10 18:00:30 +0530

    Update

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,23 +62,35 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+I = eye(num_labels);
+Y = zeros(m, num_labels);
+for i = 1:m
+  Y(i, :) = I(y(i), :);
+end
 
+A1 = [ones(m, 1) X];
+Z2 = A1 * Theta1';
+A2 = [ones(size(Z2, 1), 1) sigmoid(Z2)];
+Z3 = A2 * Theta2';
+H = A3 = sigmoid(Z3);
 
+% step 1 Feedforward and cost function
+J = (1 / m) * sum(sum((-Y) .* log(H) - (1 - Y) .* log(1 - H), 2));
 
+% step 2 Regularized cost function
+penalty = (lambda / (2 * m)) * (sum(sum(Theta1(:, 2:end) .^ 2, 2)) + sum(sum(Theta2(:, 2:end) .^ 2, 2)));
+J = J + penalty;
 
+% step 3 Neural Network Gradient (Backpropagation)
+Sigma3 = A3 - Y;
+Sigma2 = (Sigma3 * Theta2 .* sigmoidGradient([ones(size(Z2, 1), 1) Z2]))(:, 2:end);
 
+Delta_1 = Sigma2' * A1;
+Delta_2 = Sigma3' * A2;
 
-
-
-
-
-
-
-
-
-
-
-
+% step 4 Regularized Gradient
+Theta1_grad = Delta_1 ./ m + (lambda / m)*[zeros(size(Theta1, 1), 1) Theta1(:, 2:end)];
+Theta2_grad = Delta_2 ./ m + (lambda / m)*[zeros(size(Theta2, 1), 1) Theta2(:, 2:end)];
 
 % -------------------------------------------------------------
 
